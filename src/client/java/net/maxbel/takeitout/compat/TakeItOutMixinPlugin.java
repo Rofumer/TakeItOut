1d51fb82ccfa42e8c7c0c0380d42a9dd3bec025c
BlinkWhite <bilixwhite@qq.com>
2026-07-05 00:43:39 +0800

    Remove printer mixins; printer-side TakeItOutCompat handles it

--- src/client/java/net/maxbel/takeitout/compat/TakeItOutMixinPlugin.java
+++ src/client/java/net/maxbel/takeitout/compat/TakeItOutMixinPlugin.java
@@ -17,9 +17,7 @@ public class TakeItOutMixinPlugin implements IMixinConfigPlugin {
             "net.maxbel.takeitout.mixin.client.MaterialListUtilsMixin", () -> FabricLoader.getInstance().isModLoaded("litematica"),
             "net.maxbel.takeitout.mixin.client.GuiMaterialListMixin", () -> FabricLoader.getInstance().isModLoaded("litematica"),
             "net.maxbel.takeitout.mixin.client.MouseMixin", () -> FabricLoader.getInstance().isModLoaded("litematica"),
-            "net.maxbel.takeitout.mixin.client.TweakerooMixin", () -> FabricLoader.getInstance().isModLoaded("tweakeroo"),
-            "net.maxbel.takeitout.mixin.client.PrinterMixin", () -> FabricLoader.getInstance().isModLoaded("litematica_printer"),
-            "net.maxbel.takeitout.mixin.client.NewPrinterMixin", () -> FabricLoader.getInstance().isModLoaded("litematica-printer"));
+            "net.maxbel.takeitout.mixin.client.TweakerooMixin", () -> FabricLoader.getInstance().isModLoaded("tweakeroo"));
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
